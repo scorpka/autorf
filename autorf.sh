679016402cc1dae1648803b8a2ad257ca27daa4f
scorpka <ylltrabott@mail.ru>
2020-11-30 02:13:04 +0500

    added automization and hidden work

--- autorf.sh
+++ autorf.sh
@@ -1,50 +1,35 @@
 #!/bin/bash
-#/bin/bash /home/vacilica/scripts/opentg.sh
-sleep 5;
+sleep 1;
 export DISPLAY=:0;
 export XAUTHORITY=/home/vacilica/.Xauthority;
 sleep 1;
-telegram-desktop --;
-sleep 10;
-xdotool type "rf_telegram_bot";
-xdotool key Return;
-sleep 3
+telegram-desktop -- & 
+sleep 5;
+idtg=`wmctrl -l | grep -P 'Telegram' | cut -c1-10`
+xdotool type --window $idtg "rf_telegram_bot";
+xdotool key --window $idtg Return;
+sleep 1
 for ((i = 0; i < 4; i++))
 do
-#telegram-desktop --
-xdotool key Return;
-xdotool type ":clas";
+idtg=`wmctrl -l | grep -P 'Telegram' | cut -c1-10`
+xdotool key --window $idtg Return;
 sleep 1;
-xdotool key Return;
-xdotool type -delay 300 " В ген. штаб";
+xdotool type --window $idtg -delay 300 ":classical_building: В ген. штаб";
 sleep 1;
-xdotool key Return;
-sleep 140
-xdotool type -delay 300 ":sparkling h";
+xdotool key --window $idtg Return;
+sleep 140;
+xdotool type --window $idtg -delay 300 ":sparkling_heart: Пополнить здоровье";
 sleep 1;
-xdotool key Return;
+xdotool key --window $idtg Return;
 sleep 1;
-xdotool type -delay 300 " Пополнить здоровье";
-sleep 1
-xdotool key Return;
-sleep 1
-xdotool type -delay 300 "☠️ Локации";
+xdotool type --window $idtg -delay 300 "☠️ Локации";
 sleep 1;
-xdotool key Return;
+xdotool key --window $idtg Return;
 sleep 1;
-xdotool type -delay 300 ":four_c";
-sleep 1;
-xdotool key Right;
-sleep 1;
-xdotool key Return;
-xdotool type -delay 300 " 41-50 Элан";
-xdotool key Return;
-sleep 200;
-xdotool type -delay 300 ":kni";
-sleep 1;
-xdotool key Return;
-sleep 1;
-xdotool type -delay 300 " Атаковать";
-xdotool key Return;
+xdotool type --window $idtg -delay 300 ":four_leaf_clover: 41-50 Элан";
+xdotool key --window $idtg Return;
+sleep 180;
+xdotool type --window $idtg -delay 300 ":knife: Атаковать";
+xdotool key --window $idtg Return;
 sleep 10;
 done
